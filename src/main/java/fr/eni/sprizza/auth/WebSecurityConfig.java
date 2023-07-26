b8dabe5ac9093b535c73e8fe190c3b05b926dd58
Virgile-29 <v.moulart29@gmail.com>
2023-07-26 20:58:24 +0200

    Adding Springboot security impl for log in

---
+++ src/main/java/fr/eni/sprizza/auth/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package fr.eni.sprizza.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    /**
+     * This is the user details service that will be used by the authentication manager
+     * @return the user details service
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    /**
+     * This is the password encoder that will be used to encode the password
+     * @return the password encoder
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * This is the authentication provider that will be used by the authentication manager
+     * @return the authentication provider
+     */
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+
+    // TODO: Match the right url to the right role
+    /**
+     * This is the filter chain that will be used by the authentication manager
+     * @return the filter chain
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+}
