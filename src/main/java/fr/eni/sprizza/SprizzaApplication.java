f815b14a70dee71195ec151523bc42f279645b87
Herr-chirurg <panzaniguihard@gmail.com>
2023-07-26 14:21:59 +0200

    migration bdd vers sql server, ajout d'un fichier de log

--- src/main/java/fr/eni/sprizza/SprizzaApplication.java
+++ src/main/java/fr/eni/sprizza/SprizzaApplication.java
@@ -1,5 +1,7 @@
 package fr.eni.sprizza;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,23 +13,8 @@ import fr.eni.sprizza.dal.OrderRepository;
 @SpringBootApplication
 public class SprizzaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(SprizzaApplication.class, args);
 	}
 	
-	@Bean
-	public CommandLineRunner demo(OrderRepository orderRepository) {
-		
-		System.out.println("test");
-		
-		Order123 order = new Order123();
-		order.setClientName("joe");
-		
-		orderRepository.save(order);
-		
-		orderRepository.findAll().forEach(orderItem -> System.out.println(orderItem));
-		
-		return null;
-		
-	}
 }
--- src/test/java/fr/eni/sprizza/test/SprizzaApplicationTests.java
+++ src/test/java/fr/eni/sprizza/test/SprizzaApplicationTests.java
@@ -1,19 +1,33 @@
 package fr.eni.sprizza.test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 
 import fr.eni.sprizza.bo.Order123;
 import fr.eni.sprizza.dal.OrderRepository;
 
 @SpringBootTest
 class SprizzaApplicationTests {
+	
+	private OrderRepository orderRepository; 
 
+    @Autowired
+    public SprizzaApplicationTests(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+	
 	@Test
 	void contextLoads() {
 
+		System.out.println("test");
+		
+		Order123 order = new Order123();
+		order.setClientName("joe");
+		
+		orderRepository.save(order);
+		
+		orderRepository.findAll().forEach(orderItem -> System.out.println(orderItem));
 		
 	}
 
