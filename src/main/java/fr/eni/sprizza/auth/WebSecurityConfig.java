8add38e9ea3f1b44850f9f4fd5c3b21d3562952c
Virgile-29 <v.moulart29@gmail.com>
2023-07-27 19:02:24 +0200

    Adding redirect after login

--- src/main/java/fr/eni/sprizza/auth/WebSecurityConfig.java
+++ src/main/java/fr/eni/sprizza/auth/WebSecurityConfig.java
@@ -7,11 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -60,6 +57,7 @@ public class WebSecurityConfig {
                 .formLogin((form) -> form
                         .loginPage("/admin/login")
                         .permitAll()
+                        .defaultSuccessUrl("/admin", true)
                 )
                 .httpBasic(Customizer.withDefaults());
         System.out.println("Security filter chain");
