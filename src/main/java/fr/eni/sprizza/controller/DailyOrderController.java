02a7f36445aff9dc411326a91e092b8dc0d0199b
Herr-chirurg <panzaniguihard@gmail.com>
2023-08-01 13:43:17 +0200

    panel admin avant dailyOrder

--- src/main/java/fr/eni/sprizza/controller/DailyOrderController.java
+++ src/main/java/fr/eni/sprizza/controller/DailyOrderController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import fr.eni.sprizza.bll.DailyOrderComparator;
 import fr.eni.sprizza.bll.OrderService;
@@ -22,7 +24,7 @@ public class DailyOrderController {
         this.orderService = orderService;
     }
 	
-    @GetMapping("/dailyOrder")
+    @GetMapping("/admin/dailyOrder")
     public String findAllOrder( Model model) {
     	
     	List<Order> orders = orderService.findAll();
@@ -33,10 +35,12 @@ public class DailyOrderController {
         return "dailyOrder";
     }
     
-    @PostMapping("/dailyOrder")
-    public String updateStatusPay(Model model) {
+    @PostMapping("/admin/dailyOrder")
+    public String updateStatusPay(@RequestBody MultiValueMap<String, String> formData) {
     	
-    	
+    	for (String key : formData.keySet()) {
+    		
+    	}
     	
     	
 		return null;
